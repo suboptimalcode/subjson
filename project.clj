b565ee41745be8d9c8e4efa19fdbe45bd28270ed
David Santiago <david.santiago@gmail.com>
2013-04-18 13:09:11 -0700

    Update to criterium 0.4.0, can now make releases.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [criterium "0.3.2-SNAPSHOT"]]
+                 [criterium "0.4.0"]]
   :plugins [[perforate "0.3.1"]
             [lein-javadoc "0.1.0"]]
 
